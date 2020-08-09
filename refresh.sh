21d6d0d3d6da9e8ef19804ef8322a164ea820b95
seongminyoo <dbtjdals1771@ajou.ac.kr>
2020-08-09 19:18:21 +0900

    make refresh fork repository[D[D[D[D[D[D[D[D[D[D[D[D[D[auto synchronazaion fork repository

---
+++ refresh.sh
@@ -0,0 +1,62 @@
+echo "###############################################################"
+echo "#                                                             #"
+echo "#                                                             #"
+echo "#                                                             #"
+echo "#                                                             #"
+echo "#    fork repository synchronization  auto script ver 1.0     #"
+echo "#                                                             #"
+echo "#                                                             #"
+echo "#                 written by sungminyou                       #"
+echo "#                                                             #"
+echo "###############################################################"
+echo -e "\n"
+echo "*** check remote repository ***"
+echo -e "\n"
+git remote -v
+echo -e "\n"
+echo -e "add remote repository?\n"
+echo -en "[y/n] >>> "
+read option
+while (( $option != "y" || $option != "n" )); do
+    echo -en "[y/n] >>> "
+    read option
+done
+if (($option == "y")); then
+    echo -en "please type remote repository url >>> "
+    read url
+    git remote add upstream "$url"
+    echo -e "\n"
+    echo "*** check remote repository ***"
+    echo -e "\n"
+    git remote -v
+    echo -e "\n"
+    echo "*** fetch remote repository ***"
+    echo -e "\n"
+    git fetch upstream
+    echo -e "\n"
+    echo "*** merge remote/local repository ***"
+    echo -e "\n"
+    git merge upstream/master
+    echo -e "\n"
+    echo "*** push ***"
+    echo -e "\n"
+    git push origin master
+    echo -e "\n"
+    echo "*** pushing successfully done ***"
+    echo -e "\n"
+elif (($option == "n")); then
+    echo "*** fetch remote repository ***"
+    echo -e "\n"
+    git fetch upstream
+    echo -e "\n"
+    echo "*** merge remote/local repository ***"
+    echo -e "\n"
+    git merge upstream/master
+    echo -e "\n"
+    echo "*** push ***"
+    echo -e "\n"
+    git push origin master
+    echo -e "\n"
+    echo "*** pushing successfully done ***"
+    echo -e "\n"
+fi
