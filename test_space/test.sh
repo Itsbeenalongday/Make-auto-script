16b61cf8ef71842a090725255bd158d2a5981680
Itsbeeanlongday <dbtjdals1771@ajou.ac.kr>
2020-09-02 18:21:35 +0900

    update

---
+++ test_space/test.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/expect
+echo "############################################"
+echo "#                                          #"
+echo "#                                          #"
+echo "#                                          #"
+echo "#                                          #"
+echo "#    git pull push auto script ver 1.0     #"
+echo "#                                          #"
+echo "#         written by sungminyou            #"
+echo "#                                          #"
+echo "#                                          #"
+echo "#                                          #"
+echo "############################################"
+echo "\n"
+echo "*** start pull ***"
+echo "\n"
+git pull
+echo "\n"
+echo -n "please enter a commit message >> "
+read line
+echo "\n"
+expect << EOF
+spawn sudo git add .
+expect {
+    -re {\[sudo\] password for sungminyou:} {send "Coder1771?!?moneY\r"}
+}
+expect eof
+EOF
+echo "\n"
+git commit -m "$line"
+echo "\n"
+git push
+echo "\n"
+echo "*** Pushing ended successfully ***"
